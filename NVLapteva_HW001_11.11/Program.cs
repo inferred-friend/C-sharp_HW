364a8f5d9cb7ec35fbf9b54f77000a78d866e6d5
Natalia <nvlapteva@gmail.com>
2022-11-14 21:10:28 +0300

    change first file with homework and write program

---
+++ NVLapteva_HW001_11.11/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите первое целое число: ");
+int number_1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе целое число: ");
+int number_2 = Convert.ToInt32(Console.ReadLine());
+if (number_1 > number_2) Console.WriteLine($"Число {number_1} больше числа {number_2}");
+else Console.WriteLine($"Число {number_2} больше числа {number_1}");
