c37f442ad7f28283e9e9d133c67728257ee50741
Natalia <nvlapteva@gmail.com>
2022-11-14 21:27:56 +0300

    add second homework file and write program

---
+++ NVLapteva_HW002_11.11/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите первое целое число: ");
+int number_1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе целое число: ");
+int number_2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье целое число: ");
+int number_3 = Convert.ToInt32(Console.ReadLine());
+int max = number_1;
+if (number_2 > max) max = number_2;
+if (number_3 > max) max = number_3;
+Console.WriteLine($"Максимальное число {max}");
