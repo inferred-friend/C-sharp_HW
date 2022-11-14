cc32a287994f586cd62fc7cff80d36ac7bf897f2
Natalia <nvlapteva@gmail.com>
2022-11-14 22:07:32 +0300

    add fourth homework file and write program

---
+++ NVLapteva_HW004_11.11/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+while (count <= number)
+{
+    if(count % 2 == 0) Console.Write($"{count}, ");
+    count++;
+}
+
