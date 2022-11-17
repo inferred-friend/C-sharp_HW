4c6b917535ec057995301f6b09ddbc4c4c82f8ae
Natalia <nvlapteva@gmail.com>
2022-11-17 20:53:36 +0300

    add file HW005

---
+++ NVLapteva_HW005_15.11/Program.cs
@@ -0,0 +1,26 @@
+﻿
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+int number = ReadInt("Введите трехзначное число: ");
+int amount = number.ToString().Length;
+
+if (amount < 3 || amount > 3)
+{
+    Console.WriteLine("Вы ввели не трехзначное число");
+}
+else
+{
+    Console.WriteLine(InCenter(number));
+}
+
+int InCenter(int a)
+{
+    
+    int result = ((a / 10) % 10);
+    return result;
+}
