a11743ce41129d8edb454d74f18f64f571b0a29b
Natalia <nvlapteva@gmail.com>
2022-11-17 22:48:26 +0300

    add file HW007

---
+++ NVLapteva_HW007_15.11/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите номер дня недели (от 1 до 7): ");
+int day = Convert.ToInt32(Console.ReadLine());
+if (day < 1 || day > 7) Console.WriteLine("Вы ввели неверное число");
+else if (day == 6 || day == 7) Console.WriteLine("Выходной день");
+else Console.WriteLine("Рабочий день");
