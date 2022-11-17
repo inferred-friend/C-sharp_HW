6605ad45f4e0a209b5b71a85e23539a13d637872
Natalia <nvlapteva@gmail.com>
2022-11-17 21:03:37 +0300

    add file HW006

---
+++ NVLapteva_HW006_15.11/Program.cs
@@ -0,0 +1,21 @@
+﻿int ThirdDigit(int number)
+        {
+            int result = -1;
+            if (number >= 100)
+            {
+                while (number > 999)
+                {
+                    number = number / 10;
+                }
+                result = number % 10;
+            }
+            return result; 
+        }
+        
+Console.Write("Введите число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+if (ThirdDigit(number1) == -1)
+Console.WriteLine("Третьей цифры нет");
+else
+Console.WriteLine($"Третья цифра в числе: {ThirdDigit(number1)}");
