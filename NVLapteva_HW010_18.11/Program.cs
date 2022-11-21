0f1527389bc7e3c965fcb250bc69ca863c7bb3d6
Natalia <nvlapteva@gmail.com>
2022-11-21 20:54:55 +0300

    add Task23 in HW010

--- NVLapteva_HW010_18.11/Program.cs
+++ NVLapteva_HW010_18.11/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 5 ->
+// 1 | 1
+// 2 | 8
+// 3 | 27
+// 4 | 64
+// 5 | 125
+
+int N = -1;
+while (N < 1)
+{
+    Console.WriteLine("Введите любое положительное число: ");
+    N = Convert.ToInt32(Console.ReadLine());
+    if (N < 1) Console.WriteLine("Вы ввели неверные данные. Повторите ввод данных.");
+    
+}
+void Cube(int num)
+{
+    int count = 1;
+    while (count <= num)
+    {
+       Console.WriteLine($"Куб числа {count} --> {Math.Pow(count, 3)}");
+       count++;
+    }
+}
+Cube(N);
