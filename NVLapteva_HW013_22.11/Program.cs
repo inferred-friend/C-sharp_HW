123967bffb17523c7ef00c378bcf3e37280f39ce
Natalia <nvlapteva@gmail.com>
2022-11-25 08:56:40 +0300

    add Task 29 in HW013

---
+++ NVLapteva_HW013_22.11/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая задаёт массив 
+// из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+
+int[] array = new int[8];
+
+void RandomArray(int[] array)
+{
+    Random num = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = num.Next(0, 1000);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+RandomArray(array);
+PrintArray(array);
