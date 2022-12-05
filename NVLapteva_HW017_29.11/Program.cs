64f2f23f743069bfe4eba4c37f136b4ad959a4d3
Natalia <nvlapteva@gmail.com>
2022-12-05 22:10:22 +0300

    add Task 41 in HW018

---
+++ NVLapteva_HW017_29.11/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Какое количество чисел Вы хотите ввести: ");
+int numbers = Convert.ToInt32(Console.ReadLine());
+int[] Numbers(int num)
+{
+    int[] array = new int[num];
+    for (int i = 0; i < num; i++)
+    {
+        Console.WriteLine($"Введите {i+1} число: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+int Count(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) count++;
+    }
+    return count;
+}
+
+int[] array = Numbers(numbers);
+
+Console.WriteLine($"Вы ввели {Count(array)} чисел больше нуля");
