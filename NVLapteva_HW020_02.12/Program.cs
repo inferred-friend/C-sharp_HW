6c0c5c6811436b47ffff488785f942f080cd703b
Natalia <nvlapteva@gmail.com>
2022-12-13 20:27:10 +0300

    add Task 50 in HW20

---
+++ NVLapteva_HW020_02.12/Program.cs
@@ -0,0 +1,34 @@
+﻿//Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 1,7 -> такого числа в массиве нет
+
+Console.Write("Введите номер строки: ");
+int lines = Convert.ToInt32(Console.ReadLine()) - 1;
+Console.Write("Введите номер столбца: ");
+int columns = Convert.ToInt32(Console.ReadLine()) - 1;
+int n = 5; 
+int m = 7; 
+Random random = new Random();
+int[,] arr = new int[n, m];
+Console.WriteLine("Исходный массив: ");
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+    for (int j = 0; j < arr.GetLength(1); j++)
+{
+    arr[i, j] = random.Next(10, 99);
+Console.Write("{0} ", arr[i, j]);
+}
+Console.WriteLine();
+}
+    if (lines < 0 | lines > arr.GetLength(0) - 1 | columns < 0 | columns > arr.GetLength(1) - 1)
+{
+Console.WriteLine("Такого элемента в массиве нет  ");
+}
+    else
+{
+    Console.WriteLine("Значение элемента массива = {0}", arr[lines, columns]);
+}
